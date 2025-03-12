9479bc6b882a6319bcd6f3859e340adb93bc370b
Yolanda Sandoval <ysandovalt@gmail.com>
2025-03-12 20:09:05 -0300

    inicial

---
+++ src/test/java/com/grupouno/iot/minero/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupouno.iot.minero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
