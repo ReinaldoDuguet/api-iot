37d41c0c22897a4047f2e45611035d766c48f5e7
Julio <julioandrescampos@gmail.com>
2025-03-31 03:42:03 -0300

    Refactor: nueva arquitectura modular con simuladores externos

---
+++ simulators/kafka-simulator/src/main/java/com/grupouno/kafka/dto/SensorMessageDTO.java
@@ -0,0 +1,15 @@
+package com.grupouno.kafka.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SensorMessageDTO {
+    private String api_key;
+    private List<MeasurementDTO> json_data;
+}
---
+++ simulators/kafka-simulator/src/test/java/com/grupouno/AppTest.java
@@ -0,0 +1,15 @@
+package com.grupouno;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Basic unit test to validate project setup.
+ */
+public class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
